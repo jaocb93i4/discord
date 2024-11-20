4b990309daeb93be3bc88352107842e7a86becd7
jaocb93i4 <oplink586@gmail.com>
2024-11-20 15:31:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Wait for the DOM to fully load
+document.addEventListener("DOMContentLoaded", function() {
+    // Redirect to the Discord link after 3 seconds
+    setTimeout(function() {
+        window.location.href = "https://discord.gg/mm2bigcom";
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
